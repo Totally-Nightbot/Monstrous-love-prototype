fa7e9cfcc7cae83cf1d53bd415ab1abf02e8ec24
Totally-Nightbot <167849943+Totally-Nightbot@users.noreply.github.com>
2024-09-17 13:18:40 +1000

    created UI scene and title

---
+++ Assets/C# scripts/MyScripts/TitlescreenUI.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitlescreenUI : MonoBehaviour
+{
+    public Button settings;
+    public Button start;
+    public Button exit;
+    public Button chapterSelect;
+    public Button back;
+
+    public GameObject titleScreen;
+    public GameObject chapterScreen;
+    public GameObject settingsMenu;
+
+    void Start() //when these buttons are clicked they send out a message to the diffrent recivers 
+    {
+        Button op1 = settings.GetComponent<Button>();
+        op1.onClick.AddListener(settingsClicked);
+
+        Button op2 = start.GetComponent<Button>();
+        op2.onClick.AddListener(startClicked);
+
+        Button op3 = exit.GetComponent<Button>();
+        op3.onClick.AddListener(exitClicked);
+
+        Button op4 = chapterSelect.GetComponent<Button>();
+        op4.onClick.AddListener(ChapterSelectionScreen);
+
+        Button op5 = back.GetComponent<Button>();
+        op5.onClick.AddListener(BackButton);
+
+    }
+
+    void Update()
+    {
+       
+    }
+
+    void settingsClicked()
+    {
+        titleScreen.gameObject.SetActive(false);
+        settingsMenu.gameObject.SetActive(true);
+        back.gameObject.SetActive(true);
+    }
+
+    void startClicked()
+    {
+        SceneManager.LoadScene("resturant final scene");
+    }
+
+    void exitClicked()
+    {
+        Application.Quit();
+
+    }
+
+    void ChapterSelectionScreen()
+    {
+        titleScreen.gameObject.SetActive(false);
+        chapterScreen.gameObject.SetActive(true);
+        back.gameObject.SetActive(true);
+    }
+
+    void BackButton()
+    {
+        titleScreen.gameObject.SetActive(true);
+        chapterScreen.gameObject.SetActive(false);
+        settingsMenu.gameObject.SetActive(false);
+        back.gameObject.SetActive(false);
+
+    }
+}
