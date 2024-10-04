32c989fd9c5fdc290a7e8098f0442dbae3214abd
Totally-Nightbot <167849943+Totally-Nightbot@users.noreply.github.com>
2024-10-04 23:52:19 +1000

    added something special to my code uwu (go look for it)

--- Assets/Assets/C# scripts/MyScripts/DiegeticUI.cs
+++ Assets/Assets/C# scripts/MyScripts/DiegeticUI.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -38,4 +38,5 @@ public class DiegeticUI : MonoBehaviour
             Destroy(iconObject);
         }
     }
+
 }
