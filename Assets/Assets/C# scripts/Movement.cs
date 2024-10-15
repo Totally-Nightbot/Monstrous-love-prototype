b8161e47dee279488233af1577feeb6ddb994c43
Totally-Nightbot <167849943+Totally-Nightbot@users.noreply.github.com>
2024-10-15 13:31:00 +1100

    AAAAAAAAAHHHHHHHHHH

--- Assets/Assets/C# scripts/Movement.cs
+++ Assets/Assets/C# scripts/Movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 	public PhoneMenu phoneMenu;
-		public Spinch_intro interactWith;
+		public InteractWith interactWith;
 		public float walkSpeed = 5f;
 
 
