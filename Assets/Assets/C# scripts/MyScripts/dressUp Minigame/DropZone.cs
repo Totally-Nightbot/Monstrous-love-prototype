3cba95473afcf1414dbd20d3143074c478d59b3d
Totally-Nightbot <167849943+Totally-Nightbot@users.noreply.github.com>
2024-10-21 12:26:50 +1100

    worked on dress up minigame

---
+++ Assets/Assets/C# scripts/MyScripts/dressUp Minigame/DropZone.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DropZone : MonoBehaviour, IDropHandler 
+{
+   public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop to " + gameObject.name);
+    }
+}
--- Assets/Assets/C# scripts/MyScripts/dressUp Minigame/dragabble.cs
+++ Assets/Assets/C# scripts/MyScripts/dressUp Minigame/dragabble.cs
@@ -6,17 +6,28 @@ using UnityEngine.EventSystems;
 
 public class dragabble: MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    Transform parentToReturnTo = null;
   public void OnBeginDrag(PointerEventData eventdata)
     {
         Debug.Log("onBeginDrag");
+
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("onDrag");
+        //Debug.Log("onDrag");
+
+        this.transform.position = eventData.position;
+
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("onEndDrag");
+        this.transform.SetParent(parentToReturnTo);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
 }
